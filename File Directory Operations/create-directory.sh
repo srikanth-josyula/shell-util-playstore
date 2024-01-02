a7e40707290c70bd088fdaf839a01a8918e3afa1
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2024-01-02 11:04:17 -0400

    Update create-directory.sh

--- File Directory Operations/create-directory.sh
+++ File Directory Operations/create-directory.sh
@@ -3,9 +3,14 @@
 # Shell Script for Creating Directories
 DIR_NAME='testing'
 
-#Create a new directory under current folder
-mkdir $DIR_NAME
+# Check if the directory already exists
+if [ -d "$DIR_NAME" ]; then
+    echo "Directory '${DIR_NAME}' already exists."
+else
+    # Create a new directory under the current folder
+    mkdir "$DIR_NAME"
+    echo "A new directory with name '${DIR_NAME}' is created."
+fi
 
-echo "A New Directory with name '${DIR_NAME}' is created"
-
+# List the contents of the current directory
 ls -ltr
