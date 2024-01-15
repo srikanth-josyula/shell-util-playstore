fa87fe2b1f97d5202f35e58bf8f95420b4868d39
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2024-01-15 10:22:26 -0500

    Update search-files.sh

--- File Search Operations/search-files.sh
+++ File Search Operations/search-files.sh
@@ -8,6 +8,10 @@ if (find "$SEARCH_PATH" -type d -name "$SEARCH_DIR"); then
      # find file under the specified directory with format
      SEARCH_FILES=$(find "${SEARCH_PATH}/${SEARCH_DIR}" -type f -name "*.out")
      echo "Files with command found: $SEARCH_FILES"
+
+     # consider Case-insensitive version of `-name`. Searches for files with a specific name or pattern, regardless of case.
+     SEARCH_FILES1=$(find "${SEARCH_PATH}/${SEARCH_DIR}" -type f -iname "*.out")
+     echo "Files with command found: $SEARCH_FILES1"
 else
     echo "Couldn't find a directory with name ${SEARCH_DIR}"
 fi
