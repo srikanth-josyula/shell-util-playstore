3c05aaac2972d0101025d288745c83cfa68a1620
Srikanth Josyula <44222500+srikanth-josyula@users.noreply.github.com>
2024-01-18 10:25:09 -0400

    Update log-rotation.sh

--- log-rotation.sh
+++ log-rotation.sh
@@ -1,4 +1,3 @@
-
 #!/bin/bash
 
 #
@@ -12,7 +11,7 @@ LOGBACKUPDIR=/Data01/old_logs
 # Maximum FileSize of the log (in MB)
 MAXFILESIZE=20
 
-echo "Log rotation script started at $(date '+%Y-%m-%d_%H:%M:%S')"
+echo "Log rotation script started at "$(date '+%Y-%m-%d_%H:%M:%S')
 
 for LOGFILE in "$LOGDIR"/*; do
 
@@ -28,6 +27,7 @@ for LOGFILE in "$LOGDIR"/*; do
     # Check if a date folder exists
     if [ ! -d "$LOGBACKUPDIR/$DATE_FOLDER" ]; then
       # Date folder does not exist; creating one
+      echo 'Creating Date folder' "$DATE_FOLDER"
       mkdir -p "$LOGBACKUPDIR/$DATE_FOLDER"
     fi
 
@@ -35,56 +35,65 @@ for LOGFILE in "$LOGDIR"/*; do
     FILENAME=$(basename "$LOGFILE")
 
     # Move the files
+    echo 'Moving' "$FILENAME" 'to' "$LOGBACKUPDIR/$DATE_FOLDER"
     cp "$LOGFILE" "$LOGBACKUPDIR/$DATE_FOLDER"
     sudo rm "$LOGFILE"
     mv "$LOGBACKUPDIR/$DATE_FOLDER/$FILENAME" "$LOGBACKUPDIR/$DATE_FOLDER/$FILENAME.${DATE_FOLDER}_${TIME_FOLDER}"
 
-    # Set permissions regularly to alfresco user and 775
-    chown -R alfresco:alfresco "$LOGBACKUPDIR/$DATE_FOLDER"
-    chmod -R 775 "$LOGBACKUPDIR/$DATE_FOLDER"
   fi
 done
 
-echo "Zipping old logs"
-
+# Zipping old logs
 PREVDAY=$(date --date="yesterday" +"%Y-%m-%d")
-
-# Check if the previous day folder exists
 if [ -d "$LOGBACKUPDIR/$PREVDAY" ]; then
   # Compress and clear the log file
   cd "$LOGBACKUPDIR" || exit
   zip -r "$PREVDAY.zip" "$PREVDAY"
 
   # Clear archive folder
+  echo "Deleting $LOGBACKUPDIR/$PREVDAY"
   rm -rf "$LOGBACKUPDIR/$PREVDAY"
 fi
 
 # Additional Directory Operations
 
-# Create year/month/date folders
-YEAR=$(date '+%Y')
-MONTH=$(date '+%m')
-DAY=$(date '+%d')
-DATE_FOLDER="$LOGBACKUPDIR/$YEAR/$MONTH/$DAY"
-mkdir -p "$DATE_FOLDER"
+# Create a folder with year/month/date in the directory
+current_year=$(date '+%Y')
+current_month=$(date '+%m')
+current_day=$(date '+%d')
 
-# Check if it's been 3 months, then zip and delete old log folders
-THREE_MONTHS_AGO=$(date --date="3 months ago" +"%Y-%m-%d")
-if [ -d "$LOGBACKUPDIR/$THREE_MONTHS_AGO" ]; then
+year_month_date_folder="$LOGBACKUPDIR/$current_year/$current_month/$current_day"
+mkdir -p "$year_month_date_folder"
+
+# Check the file size of Catalina logs and copy to the new folder
+CATALINA_LOG="$LOGDIR/catalina.out"
+CATALINA_SIZE=$(du -m "$CATALINA_LOG" | cut -f1)
+if [ "$CATALINA_SIZE" -gt "$MAXFILESIZE" ]; then
+  cp "$CATALINA_LOG" "$year_month_date_folder"
+  sudo echo >"$CATALINA_LOG"
+fi
+
+# Set permissions to the new folder regularly
+chown -R alfresco:alfresco "$year_month_date_folder"
+chmod -R 775 "$year_month_date_folder"
+
+# After 3 months, zip the folders and delete log files
+zip_folder_date=$(date --date="3 months ago" +"%Y/%m/%d")
+zip_folder="$LOGBACKUPDIR/$zip_folder_date"
+if [ -d "$zip_folder" ]; then
   cd "$LOGBACKUPDIR" || exit
-  zip -r "$THREE_MONTHS_AGO.zip" "$THREE_MONTHS_AGO"
-  rm -rf "$LOGBACKUPDIR/$THREE_MONTHS_AGO"
+  zip -r "$zip_folder_date.zip" "$zip_folder_date"
+  rm -rf "$zip_folder_date"
 fi
 
-# Check if it's been 6 months, then move to temp folder
-SIX_MONTHS_AGO=$(date --date="6 months ago" +"%Y-%m-%d")
-if [ -d "$LOGBACKUPDIR/$SIX_MONTHS_AGO" ]; then
-  HALF_YEARLY_FOLDER="$LOGBACKUPDIR/temp/half_yearly-$YEAR"
-  mkdir -p "$HALF_YEARLY_FOLDER"
-  mv "$LOGBACKUPDIR/$SIX_MONTHS_AGO" "$HALF_YEARLY_FOLDER"
+# After 6 months, move to temp folder with a suffix
+temp_suffix=$(date --date="6 months ago" +"half_yearly-%Y")
+temp_folder="/path/to/temp_directory/$temp_suffix"
+if [ -d "$zip_folder" ]; then
+  mv "$zip_folder" "$temp_folder"
 fi
 
-# Clear any empty folders found
+# Clear any empty folders later found
 find "$LOGBACKUPDIR" -type d -empty -delete
 
 echo "Script completed successfully!"
